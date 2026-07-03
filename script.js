79240384cf09ba6f92ffcb860aedf1a3f12b2d82
Shahaan <alamshahaan12@gmail.com>
2026-07-03 10:27:50 +0500

    I guess last commit?

---
+++ script.js
@@ -0,0 +1,19 @@
+const toggleImg = document.getElementById("toggle");
+const rootElement = document.documentElement; // This grabs the <html> tag
+const toggleButton = document.getElementById("toggleButton");
+const logo = document.getElementById("Logo")
+
+function changeColor() {
+    // 1. Toggle the 'dark' class on the HTML tag
+    rootElement.classList.toggle("dark");
+    
+    // 2. Bonus: Swap the toggle image asset when clicked!
+    if (rootElement.classList.contains("dark")) {
+        toggleImg.src = "Images/toggleOn.png";
+        logo.src = "Images/Logos/8.png"
+    } else {
+        toggleImg.src = "Images/toggleOff.png";
+    }
+}
+
+toggleButton.addEventListener("click", changeColor)
